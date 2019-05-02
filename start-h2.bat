de875d3b5c97ff5220bb48cad85b1544887673b2
Qinghe Wu <wuqh5@lenovo.com>
2019-05-02 23:24:23 +0800

    U

--- start-h2.bat
+++ start-h2.bat
@@ -1,4 +1,4 @@
-@echo off
-%1 mshta vbscript:CreateObject("WScript.Shell").Run("%~s0 ::",0,FALSE)(window.close)&&exit
+rem %1 mshta vbscript:CreateObject("WScript.Shell").Run("%~s0 ::",0,FALSE)(window.close)&&exit
 
 java -jar bin/ss-h2-2.0.6.RELEASE.jar --devMode=false 
+
